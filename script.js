4adffd05b767c6b49996d251cc404ccfe64f976e
Oleg <olegmodenov@gmail.com>
2020-03-12 16:09:32 +0500

    Сделана кнопка "больше"

--- script.js
+++ script.js
@@ -57,3 +57,27 @@ const togglePopup = () => {
 }
 
 togglePopup();
+
+const moreBlocks = () => {
+
+  const moreBtn = document.querySelector('.add-sentence-btn');
+  const hiddenBlocks = document.querySelectorAll('.sentence-hidden');
+
+  moreBtn.addEventListener('click', (event) => {
+
+    moreBtn.style.display = 'none';
+
+    hiddenBlocks.forEach(block => {
+
+      if (block.classList.contains('hidden')) {
+        block.classList.remove('hidden');
+      };
+      
+      if (block.classList.contains('visible-sm-block')) {
+        block.classList.remove('visible-sm-block');
+      };
+    })
+  });
+}
+
+moreBlocks();
